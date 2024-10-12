32b4d65daa4540c3548dee2bd71ec893053b6c8a
IhGori <igor.vinicius@discente.univasf.edu.br>
2024-10-12 13:57:37 -0300

    feat: Atualiza metodo de test realativo a recruitment

--- tests/test_create_vacancy.py
+++
@@ -1,6 +0,0 @@
-from pages.modules.recruitment.recruitmentPage import RecruitmentPage
-
-def test_create_vacancy(logged_in_page):
-	vacancies_p = RecruitmentPage(logged_in_page)
-	vacancies_p.access_module()
-	vacancies_p.access_vacancies()
