73c8d15f73588247af7c86c8074bfe9e9ca9d607
Allan Cleysson <allancleysson@gmail.com>
2024-10-15 17:33:06 -0300

    refactor: aplicando reuso nos testes ja criados

--- tests/test_post.py
+++ tests/test_post.py
@@ -2,7 +2,7 @@ from pages.buzzPage import BuzzPage
 
 def test_post(logged_in_page):
 	buzz_p = BuzzPage(logged_in_page)
-	buzz_p.access_module()
+	buzz_p.access_module("Buzz")
 	buzz_p.create_post()
 	buzz_p.test_like_post()
 	buzz_p.delete_post()
