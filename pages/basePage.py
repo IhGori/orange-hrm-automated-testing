7a0d9b606ddb072ce5a4cd22797cbe289de6e18a
Allan Cleysson <allancleysson@gmail.com>
2024-10-15 17:06:58 -0300

    ajustes na basePage pra contemplar 2 novas funçoes e aumentar reuso

--- pages/basePage.py
+++ pages/basePage.py
@@ -2,7 +2,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class BasePage:
+class BasePage:    
+	module_item = (By.CSS_SELECTOR, ".oxd-main-menu-item-wrapper")
+	toast_response = (By.CSS_SELECTOR, "div.oxd-toast--success")
+	list_of_modules = (By.CSS_SELECTOR, ".oxd-main-menu")
 	def __init__(self, driver):
 		self.driver = driver
 
@@ -28,3 +31,22 @@ class BasePage:
 		
 	def send_keys_to_element(self, locator, text):
 		self.driver.find_element(*locator).send_keys(text)
+
+	def access_module(self, moduleName):
+		WebDriverWait(self.driver, 10).until(
+			EC.visibility_of_element_located(self.list_of_modules)
+		)
+
+		modules = self.driver.find_elements(*self.module_item)
+		for module in modules:
+			if module.text == moduleName:
+				WebDriverWait(self.driver, 5).until(
+					EC.element_to_be_clickable(module)
+				).click()
+				break
+
+	def verify_toast_message(self, msg):
+		success_toast = self.await_element(*self.toast_response)
+		toast_message = success_toast.find_element(By.CLASS_NAME, "oxd-text--toast-message").text
+
+		assert toast_message == msg, "mensagem inválida"
