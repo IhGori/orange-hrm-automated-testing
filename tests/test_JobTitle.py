84cca777178883af0c2880b16d5d798b7801ac50
Allan Cleysson <allancleysson@gmail.com>
2024-10-15 23:01:08 -0300

    tests: add dois cenarios de teste

--- tests/test_JobTitle.py
+++ tests/test_JobTitle.py
@@ -1,7 +1,22 @@
 from pages.adminPage import AdminPage
 
-def test_JobTitle(logged_in_page):
+def test_AddJobTitle(logged_in_page):
     admin_p = AdminPage(logged_in_page)
+    createAndVerifyJobTitle(admin_p)
+    #limpando massa pra rodar os outros cenários com sucesso
+    admin_p.delete_job_title()
+    
+def test_DeleteJobTitle(logged_in_page):
+    admin_p = AdminPage(logged_in_page)
+    createAndVerifyJobTitle(admin_p)
+    admin_p.delete_job_title()
+
+def test_EditJobTitle(logged_in_page):
+    admin_p = AdminPage(logged_in_page)
+    createAndVerifyJobTitle(admin_p)
+    admin_p.edit_job_title()
+
+def createAndVerifyJobTitle(admin_p):
     admin_p.access_module("Admin")
     admin_p.load_add_job_title()
     admin_p.fill_job_fields()
